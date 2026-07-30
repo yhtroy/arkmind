715b0716b8c539dea05fb48a0af97405bb696d0b
YuHongTao <yhtroy27@gmail.com>
2026-07-30 19:27:32 +0800

    feat(asset): add arkmind-asset CLI and AssetExtractor seam (TASK-002.5)

--- src/arkmind/asset/__init__.py
+++ src/arkmind/asset/__init__.py
@@ -1,5 +1,6 @@
 from arkmind.asset.asset import Asset
 from arkmind.asset.asset_type import AssetType
+from arkmind.asset.extractor import AssetExtractor
 from arkmind.asset.repository import AssetRepository
 
-__all__ = ["Asset", "AssetRepository", "AssetType"]
+__all__ = ["Asset", "AssetExtractor", "AssetRepository", "AssetType"]
